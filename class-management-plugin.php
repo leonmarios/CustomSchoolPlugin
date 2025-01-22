691fcf71d9ef4e2ef0d7fecd193e7f426530e523
leonmarios <leonmarios@gmail.com>
2025-01-22 17:40:23 +0200

    Update class-management-plugin.php

--- class-management-plugin.php
+++ class-management-plugin.php
@@ -24,6 +24,15 @@ require_once CMP_PLUGIN_DIR . 'includes/helper-functions.php';
 require_once CMP_PLUGIN_DIR . 'includes/create-user.php';
 require_once CMP_PLUGIN_DIR . 'includes/manage-schedule.php';
 
+// Enqueue CSS and JS
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style('cmp-styles', CMP_PLUGIN_URL . 'assets/css/style.css', [], '1.0');
+    wp_enqueue_script('cmp-scripts', CMP_PLUGIN_URL . 'assets/js/script.js', ['jquery'], '1.0', true);
+});
+
+// Register shortcode for creating users
+add_shortcode('create_user_form', ['CreateUser', 'create_user_form']);
+
 // Activation hook for creating tables
 register_activation_hook(__FILE__, ['DBTables', 'create_tables']);
 ?>
