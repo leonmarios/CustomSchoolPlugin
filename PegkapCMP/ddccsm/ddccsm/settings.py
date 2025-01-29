c4383107f718b456c62910e78d89c834d78e477d
leonmarios <53005540+leonmarios@users.noreply.github.com>
2025-01-29 11:34:54 +0200

    Update settings.py

--- PegkapCMP/ddccsm/ddccsm/settings.py
+++ PegkapCMP/ddccsm/ddccsm/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(os.path.dirname(__file__), "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
